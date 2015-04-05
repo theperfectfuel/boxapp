c400112bc143cd41db1d4bf90db0124d3cab1f78
theperfectfuel <robert@theperfectfuel.com>
2015-04-05 00:32:13 -0700

    trying another db config in settings.py

--- boxapp/settings.py
+++ boxapp/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'boxapp.wsgi.application'
 
     # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
