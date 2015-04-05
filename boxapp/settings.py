23fc738b0d4e0a86723a46f02a78bbef855c3c60
theperfectfuel <robert@theperfectfuel.com>
2015-04-05 00:25:43 -0700

    still working on db setup for heroku

--- boxapp/settings.py
+++ boxapp/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
