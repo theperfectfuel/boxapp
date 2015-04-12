fe8a1c3d41d69cda58a0571ccea5c450fea61bdb
theperfectfuel <robert@theperfectfuel.com>
2015-04-12 14:05:40 -0700

    added google-analytics functionality

--- boxapp/settings.py
+++ boxapp/settings.py
@@ -16,8 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-ON_HEROKU = 'DYNO' in os.environ
+GOOGLE_ANALYTICS_TRACK_PAGE_LOAD_TIME = True
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,6 +58,7 @@ INSTALLED_APPS = (
     'bootstrap3',
     'geoposition',
     'bootstrap_pagination',
+    'google-analytics',
 )
 
 MIDDLEWARE_CLASSES = (
