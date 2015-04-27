db5b8854c55efc5129a020d18dff512346ac4c8d
theperfectfuel <robert@theperfectfuel.com>
2015-04-27 01:58:39 -0700

    changed pagination to 10 per page / updated some css

--- core/views.py
+++ core/views.py
@@ -14,7 +14,7 @@ class LandingView(TemplateView):
 class LocationListView(ListView):
 	model = coremodels.Location
 	template_name = 'location/list.html'
-	paginate_by = 5
+	paginate_by = 10
 
 class SearchListView(LocationListView):
 
